3870321400df189e5aa5c78b0129bafa29374016
enheng <enheng@enheng.com>
2019-01-11 08:46:40 +0800

    a

--- src/main/java/com/maotai/testweb/App.java
+++ src/main/java/com/maotai/testweb/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! i am maotai" );
+       System.out.println( "Hello World" );
+       System.out.println( "JAVA是全世界最垃圾的语言没有之一" );
+       System.out.println( "<<JAVA从入门到放弃>>" );
     }
 }
